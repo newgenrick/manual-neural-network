00539853481d2b10c62f7759add702d5a6368855
newgenrick <rick.awas@gmail.com>
2018-01-26 00:39:10 +0530

    Session
    created a new session function that will run the computaitions in the graph.And also the traverse postorder function.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class Placeholder():
         self.output_nodes = []
         _default_graph.placeholders.append(self)
 
-class Variables():
+class Variable():
     def __init__(self,initial = None):
         self.output_nodes = []
         self.value = initial
@@ -64,7 +64,36 @@ class Graph():
     def set_defaut_graph(self):
         global _default_graph
         _default_graph = self
-
-
-
+'''traverse postorder is a very interesting function ,in which we get an 
+    operation node as an argument and we trace it backwords to cal all the 
+    values(inputs) to that node in the correct order'''
+def traverse_postorder(operation):
+    nodes_postorder = []
+    def recurse(node):
+        if isinstance(node, operations):
+            for input_node in node.input_nodes:
+                recurse(input_node)
+        nodes_postorder.append(node)
+    recurse(operation)
+    return nodes_postorder
         
+    
+'''A session runs the graph data transfer and computation when triggered on 
+    a operational node'''
+class Session():
+    def run(self,operation,feed_dict = {}):
+        nodes_postorder = traverse_postorder(operation)
+        print(nodes_postorder)
+        
+        for node in nodes_postorder:
+            if type(node) == Placeholder:
+                node.output = feed_dict[node]
+            elif type(node) == Variable:
+                node.output = node.value
+            else:
+                node.inputs = [input_node.output for input_node in node.input_nodes]
+                node.output = node.calculate(*node.inputs)
+            if type(node.output) == list:
+                node.output = np.array(node.output)
+        return operation.output
+                
